b8a5f5e0625ce307d6d47bdc78462a8ef8147f8a
Amr Khalifa <killua96@outlook.com>
2019-01-31 00:12:19 +0200

    created SVM with custom Kernel

---
+++ models/GradientBoosting.py
@@ -0,0 +1,35 @@
+import data_reader
+from sklearn.metrics import*
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import *
+from sklearn.tree import *
+
+
+x, y = data_reader.read_data()
+
+#x = (x-x.mean(axis = 0))/x.std(axis=0)
+y = y.flatten()
+x = x.T
+
+for i in range (y.shape[0]):
+    if y[i] == -1:
+        y[i] =0
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+
+bdt = GradientBoostingClassifier(n_estimators=500, max_depth=50, learning_rate=0.1)
+
+
+bdt.fit(x_train, y_train)
+
+y_pred = bdt.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+auc = roc_auc_score(y_test, y_pred)
+
+print(y_pred)
+print("Accuracy: %.4f%%" % (accuracy * 100.0))
+
+print("AUC %.4f%%" %(auc*100.0))
--- models/SVM_custom_kernel.py
+++ models/SVM_custom_kernel.py
@@ -1,13 +1,12 @@
 import data_reader
 from sklearn.metrics import*
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import *
-from sklearn.tree import *
-
+from sklearn  import svm
+import numpy as np
 
 x, y = data_reader.read_data()
 
-x = (x-x.mean(axis = 0))/x.std(axis=0)
+#x = (x-x.mean(axis = 0))/x.std(axis=0)
 y = y.flatten()
 x = x.T
 
@@ -19,12 +18,18 @@ for i in range (y.shape[0]):
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 
-bdt = GradientBoostingClassifier(n_estimators=100)
+def my_kernel(X, Y):
+
+    return np.dot(X,Y.T)
 
 
-bdt.fit(x_train, y_train)
+h = .02  # step size in the mesh
 
-y_pred = bdt.predict(x_test)
+# we create an instance of SVM and fit out data.
+clf = svm.SVC(kernel=my_kernel)
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 auc = roc_auc_score(y_test, y_pred)
