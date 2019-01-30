71e8f5e890c6dd687d859e748f07a6bbea890bbc
Amr Khalifa <killua96@outlook.com>
2019-01-31 00:27:59 +0200

    used mutual information kerenl as similiarity measure in svm custom kernel

--- models/SVM_custom_kernel.py
+++ models/SVM_custom_kernel.py
@@ -3,7 +3,7 @@ from sklearn.metrics import*
 from sklearn.model_selection import train_test_split
 from sklearn  import svm
 import numpy as np
-
+from sklearn.feature_selection import *
 x, y = data_reader.read_data()
 
 #x = (x-x.mean(axis = 0))/x.std(axis=0)
@@ -19,6 +19,12 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 
 def my_kernel(X, Y):
+
+    K = np.zeros((X.shape[0],Y.shape[0]))
+    for i in range (X.shape[0]):
+        for j in range (Y.shape[0]):
+            K[i][j] = normalized_mutual_info_score(X[i], Y[j])
+    print(K)
 
     return np.dot(X,Y.T)
 
